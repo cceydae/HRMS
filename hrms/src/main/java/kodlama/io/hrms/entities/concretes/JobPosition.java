e0caaed8e9bb4a7b66dc3b7c9933717e8edcca47
Ceyda Eser <ceydae.1040@gmail.com>
2021-06-26 03:26:21 +0300

    hrms project

---
+++ hrms/src/main/java/kodlama/io/hrms/entities/concretes/JobPosition.java
@@ -0,0 +1,29 @@
+package kodlama.io.hrms.entities.concretes;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="job_positions")
+public class JobPosition {
+	
+	@Id
+	@GeneratedValue
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="position")
+	private String positionName;
+	
+	public JobPosition(int id, String positionName) {
+		super();
+		this.id = id;
+		this.positionName = positionName;
+	}
+}
